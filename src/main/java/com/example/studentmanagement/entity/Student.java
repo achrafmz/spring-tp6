e5846bf14897da2dcbb7687757176b96e98f1b6c
achrafmz <achrafmazouz50@gmail.com>
2025-10-25 00:48:09 +0100

    asd

---
+++ src/main/java/com/example/studentmanagement/entity/Student.java
@@ -0,0 +1,17 @@
+package com.example.studentmanagement.entity;
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private String prenom;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+
+    // Getters et Setters
+}
